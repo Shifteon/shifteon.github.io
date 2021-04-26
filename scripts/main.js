2866b57f19e8a0c40f81cebaf1ef556e3c2c2cc4
Shifteon <bpwyatt04@gmail.com>
2021-04-26 16:50:15 -0600

    Moved the script tag

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,4 @@ const date = new Date();
 const year = date.getFullYear();
 
 document.querySelector('#copyrightyear').innerHTML = year;
-
 document.querySelector('#lastupdated').innerHTML = document.lastModified;
