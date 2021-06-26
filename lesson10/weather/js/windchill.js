78548cd4e67c812f7f3a7ec082cd01b912bb5734
Shifteon <bpwyatt04@gmail.com>
2021-06-25 21:08:50 -0600

    Added dynamic weather. Fixed windchill

--- lesson10/weather/js/windchill.js
+++ lesson10/weather/js/windchill.js
@@ -6,8 +6,8 @@ window.addEventListener('load', () => {
 
     if (tempature <= 50 && windSpeed > 3) {
         windChill = Math.round(35.74 + 0.6215 * tempature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * tempature * Math.pow(windSpeed, 0.16));
-        element.innerHTML = windChill;
+        element.innerHTML = windChill + "°F";
     } else {
-        element.innerHTML = tempature;
+        element.innerHTML = "N/A";
     }
 })
