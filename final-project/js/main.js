4fe2ce0e2581c0d70da253fbe7059e252176b443
Shifteon <bpwyatt04@gmail.com>
2021-07-20 11:59:57 -0600

    Services page, date on footer

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -19,7 +19,18 @@ function getTemples() {
         });
 }
 
+function getDate() {
+    // Set the date on the footer
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date();
+    const day = days[date.getDay()];
+    const month = months[date.getMonth()];
+    document.querySelector('#footer-date').textContent = day + ", " + date.getDate() + " " + month + " " + date.getFullYear();
+}
+
 window.addEventListener('load', () => {
     openNav();
+    getDate();
     getTemples();
 });
