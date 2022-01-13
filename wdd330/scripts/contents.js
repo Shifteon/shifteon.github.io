978b03cc19919dd7b880e3c4aaf79f0319f1912e
Shifteon <bpwyatt04@gmail.com>
2022-01-12 22:54:43 -0700

    Notes and prism

--- wdd330/scripts/contents.js
+++ wdd330/scripts/contents.js
@@ -1,13 +1,16 @@
-const links = [
-    {
-      label: "Week 1",
-      url: "week1/index.html"
-    }
-  ]
+const links = [{
+    label: "Week 1",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week 2",
+    url: "week2/index.html"
+  }
+];
 let list = document.querySelector("ol");
 
 for (link of links) {
-    let item = document.createElement("li");
-    item.innerHTML = `<a href="${link["url"]}">${link["label"]}</a>`;
-    list.appendChild(item);
+  let item = document.createElement("li");
+  item.innerHTML = `<a href="${link["url"]}">${link["label"]}</a>`;
+  list.appendChild(item);
 }
