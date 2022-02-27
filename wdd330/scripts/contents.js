a0c6651d5a144748cabc6ffa7a9cb0791dcb4233
Shifteon <bpwyatt04@gmail.com>
2022-02-26 20:29:47 -0700

    week 8

--- wdd330/scripts/contents.js
+++ wdd330/scripts/contents.js
@@ -25,11 +25,15 @@ const links = [{
   {
     label: "Week 7",
     url: "week7/index.html"
+  },
+  {
+    label: "Week 8",
+    url: "week8/index.html"
   }
 ];
 let list = document.querySelector("ol");
 
-for (link of links) {
+for (let link of links) {
   let item = document.createElement("li");
   item.innerHTML = `<a href="${link["url"]}">${link["label"]}</a>`;
   list.appendChild(item);
